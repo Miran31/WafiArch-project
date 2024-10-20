66d5c92e21965e3baefad91ce3ed61116e3f3c52
miran <armiran46@gmail.com>
2024-10-20 19:00:51 +0600

    done

--- WafiArch.Api/Program.cs
+++ WafiArch.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using System;
 using WafiArch.Application.Mapping;
 using WafiArch.Application.Products;
@@ -17,6 +18,20 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
 );
 builder.Services.AddControllers();
+
+
+
+// Configure Serilog for logging
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+
+
 // Register AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddScoped<IProductAppService, ProductAppService>();
